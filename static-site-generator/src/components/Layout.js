b9d33cb91299e0a656db744a3ba2363a93f9dc7f
DanSnow <dododavid006@gmail.com>
2020-09-20 23:04:55 +0800

    add(ssg): add 05 example code

--- static-site-generator/src/components/Layout.js
+++ static-site-generator/src/components/Layout.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
-export function Layout({ title, children }) {
+export function Layout({ location, title, children }) {
+  const isHome = location.pathname === '/'
+
   return (
     <div className="max-w-screen-sm mx-auto">
       <header>
-        <h1 className="text-4xl text-center font-bold mb-8">{title}</h1>
+        <a href="/">
+          {isHome ? (
+            <h1 className="text-4xl text-center font-bold mb-8">{title}</h1>
+          ) : (
+            <h3 className="text-3xl text-center font-bold mb-8">{title}</h3>
+          )}
+        </a>
       </header>
       <main>{children}</main>
     </div>
--- static-site-generator/src/index.js
+++ static-site-generator/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import React from 'react'
 import { renderToString, renderToStaticMarkup } from 'react-dom/server'
 import { App } from './components/App'
+import slugify from 'slugify'
 
 const app = express()
 
@@ -14,27 +15,46 @@ const articles = [
     title: 'First Post',
     content: 'My first post',
   },
-]
+].map((article) =>
+  article.slug
+    ? article
+    : {
+        slug: slugify(article.title, { lower: true }),
+        ...article,
+      }
+)
 
-app.get('/', (_req, res) => {
-  res.send(
-    renderToStaticMarkup(
-      <html>
-        <head>
-          <title>My Blog</title>
-          <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
-        </head>
-        <body>
-          <div
-            id="root"
-            dangerouslySetInnerHTML={{ __html: renderToString(<App title={'My Blog'} articles={articles} />) }}
-          />
-        </body>
-      </html>
-    )
-  )
+app.get('/', (req, res) => {
+  res.send(renderHTML(toLocation(req)))
+})
+
+app.get('/articles/:slug', (req, res) => {
+  res.send(renderHTML(toLocation(req)))
 })
 
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000')
 })
+
+function renderHTML(location) {
+  return renderToStaticMarkup(
+    <html>
+      <head>
+        <title>My Blog</title>
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet" />
+      </head>
+      <body>
+        <div
+          id="root"
+          dangerouslySetInnerHTML={{
+            __html: renderToString(<App title={'My Blog'} location={location} articles={articles} />),
+          }}
+        />
+      </body>
+    </html>
+  )
+}
+
+function toLocation(req) {
+  return { pathname: req.path, params: req.params }
+}
